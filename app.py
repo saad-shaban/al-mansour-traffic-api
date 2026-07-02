7c4882e72f7cd0249d9422d482b9e2d36bc80d66
saad-shaban <susa.shaban@gmail.com>
2026-07-02 15:17:58 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,22 @@ import streamlit as st
 import requests
 from streamlit_geolocation import streamlit_geolocation
 
+with st.spinner("Analyzing road data..."):
+            try:
+                hour = 12 
+                api_url = f"https://al-mansour-traffic-api.onrender.com/predict-by-location?lat={lat}&lon={lon}&hour={hour}"
+                response = requests.get(api_url, timeout=15) # Increased timeout
+                
+                if response.status_code == 200:
+                    result = response.json()
+                    st.metric(label="Traffic Status", value=result['traffic_level'].upper())
+                else:
+                    st.error(f"API Error: {response.status_code} - {response.text}")
+                    
+            except Exception as e:
+                st.error(f"Connection Error: {str(e)}")
+
+
 # Set page configuration for a "Waze-like" professional look
 st.set_page_config(page_title="Traffic Sentinel", page_icon="🚗")
 
